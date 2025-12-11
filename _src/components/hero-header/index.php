eb5190bf8c593ee72aa9896a508ebd64a573a13c
Tommy Ferry <tommy@wholegraindigital.com>
2025-12-11 18:32:06 +0000

    adjust hero header basic layout

--- _src/components/hero-header/index.php
+++ _src/components/hero-header/index.php
@@ -1,13 +1,15 @@
 <div <?= \Granola\Helpers::build_attributes($args['attributes']); ?>>
-    <?php if (!empty($args['strapline'])) { ?>
-        <div class="hero-header__strapline-wrapper">
-            <?= \Granola\Component::get('heading', $args['strapline']); ?>
-        </div>
-    <?php } ?>
+    <div class="hero-header__inner">
+        <?php if (!empty($args['strapline'])) { ?>
+            <div class="hero-header__strapline-wrapper">
+                <?= \Granola\Component::get('heading', $args['strapline']); ?>
+            </div>
+        <?php } ?>
 
-    <?php if (!empty($args['image'])) { ?>
-        <div class="hero-header__image">
-            <?= \Granola\Component::get('image', $args['image']); ?>
-        </div>
-    <?php } ?>
+        <?php if (!empty($args['image'])) { ?>
+            <div class="hero-header__image">
+                <?= \Granola\Component::get('image', $args['image']); ?>
+            </div>
+        <?php } ?>
+    </div>
 </div>
