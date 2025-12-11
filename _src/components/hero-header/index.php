29240806d145fcf042f3878deae8ba0e30b7cb90
Tommy Ferry <tommy@wholegraindigital.com>
2025-12-11 18:35:31 +0000

    adjust hero header image overlap

--- _src/components/hero-header/index.php
+++ _src/components/hero-header/index.php
@@ -1,13 +1,13 @@
 <div <?= \Granola\Helpers::build_attributes($args['attributes']); ?>>
     <div class="hero-header__inner">
-        <?php if (!empty($args['strapline'])) { ?>
-            <div class="hero-header__strapline-wrapper">
-                <?= \Granola\Component::get('heading', $args['strapline']); ?>
-            </div>
-        <?php } ?>
-
         <?php if (!empty($args['image'])) { ?>
             <div class="hero-header__image">
+                <?php if (!empty($args['strapline'])) { ?>
+                    <div class="hero-header__strapline-wrapper">
+                        <?= \Granola\Component::get('heading', $args['strapline']); ?>
+                    </div>
+                <?php } ?>
+
                 <?= \Granola\Component::get('image', $args['image']); ?>
             </div>
         <?php } ?>
