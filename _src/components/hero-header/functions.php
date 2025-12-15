ff6a49331c6b0f2866227abc8a48edc475e4e053
Tommy Ferry <tommy@wholegraindigital.com>
2025-12-15 12:59:44 +0000

    make heading h1 if strapline not set

--- _src/components/hero-header/functions.php
+++ _src/components/hero-header/functions.php
@@ -30,11 +30,15 @@ function filter_args(array $args): ?array
         $args['strapline'] = [
             'content' => $args['strapline'],
             'classes' => ['hero-header__strapline'],
+            'el' => 'h1',
         ];
 
         if (!empty($args['image'])) {
             $args['attributes']['style']['--hero-header--image'] = 'url(' . $args['image']['url'] . ')';
         }
+    } elseif (!empty($args['heading'])) {
+        // Make heading <h1> if strapline not set.
+        $args['heading']['el'] = 'h1';
     }
 
     // -------------------------------------------------------------------------
