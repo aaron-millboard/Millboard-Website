57d6cf9e126346c4f779dde620dce414390f2132
Tommy Ferry <tommy@wholegraindigital.com>
2025-12-16 14:44:12 +0000

    move component video functions to theme utils class

---
+++ Theme/Utils/Videos.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Theme\Utils;
+
+class Videos
+{
+    /**
+     * Extract video embed URL from YouTube or Vimeo URL
+     *
+     * @param string $url Video URL
+     * @return string|false Embed URL or false if invalid.
+     */
+    public static function get_video_embed_url($url): string|false
+    {
+        // Try Vimeo
+        $vimeo_id = self::extract_vimeo_id($url);
+        if ($vimeo_id) {
+            return 'https://player.vimeo.com/video/' . $vimeo_id . '?autoplay=1';
+        }
+
+        // Try YouTube
+        $youtube_id = self::extract_youtube_id($url);
+        if ($youtube_id) {
+            return 'https://www.youtube.com/embed/' . $youtube_id . '?autoplay=1&modestbranding=1&rel=0';
+        }
+
+        return false;
+    }
+
+    /**
+     * Extract YouTube video ID from URL
+     *
+     * @param string $url Potential YouTube video URL
+     * @return string|false Video ID or false if not found.
+     */
+    public static function extract_youtube_id($url): string|false
+    {
+        $pattern = '/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i';
+        if (preg_match($pattern, $url, $matches)) {
+            return $matches[1];
+        }
+        return false;
+    }
+
+    /**
+     * Extract Vimeo video ID from URL.
+     *
+     * @param string $url Potential Vimeo video URL.
+     * @return string|false Video ID or false if not found.
+     */
+    public static function extract_vimeo_id($url): string|false
+    {
+        $pattern = '/vimeo\.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/';
+        if (preg_match($pattern, $url, $matches)) {
+            return $matches[3];
+        }
+        return false;
+    }
+}
