e8cfb67ea7d98469bb5c876b3e0d2faeb6c473a3
Tommy Ferry <tommy@wholegraindigital.com>
2025-12-18 11:18:59 +0000

    modify site-main to allow for missing page-headers on pages

--- _src/components/site-main/functions.php
+++ _src/components/site-main/functions.php
@@ -28,18 +28,16 @@ function filter_args(array $args): ?array
         // Display default header if one isn't added in the content.
         $template_page = \Granola\WordPress\TemplatePage::get_template_page($args['object']);
 
-        if (!is_single() && $template_page) {
-            if (!\has_block('acf/page-header', $template_page)) {
+        if (!\is_single() && !empty($template_page)) {
+            if (!\has_block('acf/page-header', $template_page) || !\has_block('acf/hero-header', $template_page)) {
                 $args['header'] = \Granola\Component::get('page-header', [
                     'object' => $args['object'],
                 ]);
             }
-        } else {
-            if (!\has_block('acf/page-header')) {
-                $args['header'] = \Granola\Component::get('page-header', [
-                    'object' => $args['object'],
-                ]);
-            }
+        } elseif (!\has_block('acf/page-header') || !\has_block('acf/hero-header')) {
+            $args['header'] = \Granola\Component::get('page-header', [
+                'object' => $args['object'],
+            ]);
         }
 
         if (empty($args['id']) && empty($args['attributes']['id'])) {
