a151d142a622ea730c3edee1bf21c45f5933eba7
Tommy Ferry <tommy@wholegraindigital.com>
2026-01-03 22:16:52 +0000

    update page header heading on search results page

--- _src/components/page-header/functions.php
+++ _src/components/page-header/functions.php
@@ -75,12 +75,11 @@ function filter_args(array $args): ?array
             }
 
             if (!empty($object->query['s'])) {
-                $args['preheading'] = \__('Search', 'granola');
                 $args['heading'] = sprintf(
                     \_n(
                         // translators: 1: quantity of comments. 2: post title.
-                        'Showing %1$s search result for \'%2$s\'',
-                        'Showing %1$s search results for \'%2$s\'',
+                        'Displaying %1$s search result for \'%2$s\'',
+                        'Displaying %1$s search results for \'%2$s\'',
                         $object->found_posts,
                         'granola'
                     ),
