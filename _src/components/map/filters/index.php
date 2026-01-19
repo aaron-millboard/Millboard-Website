a91ccf938f10872bd18a60f552e4178a4b857e60
Tommy Ferry <tommy@wholegraindigital.com>
2026-01-19 22:25:04 +0000

    clean up map filters template

--- _src/components/map/filters/index.php
+++ _src/components/map/filters/index.php
@@ -4,7 +4,11 @@
             <?php foreach ($args['filters'] as $filter_group) { ?>
                 <?php $filter_group_name = 'map-filtergroup-' . $filter_group['id']; ?>
 
-                <fieldset name="<?= $filter_group_name; ?>" form="map_filters_form" class="map__filters__fieldset">
+                <fieldset
+                    name="<?= esc_attr($filter_group_name); ?>"
+                    form="map_filters_form"
+                    class="map__filters__fieldset"
+                >
                     <?= \Granola\Component::get('heading', [
                         'el' => 'h3',
                         'heading' => $filter_group['label'],
@@ -17,23 +21,22 @@
                             $filter_field_name = 'map-filter-' . $filter_group['id'];
                             $filter_field_id = $filter_field_name . '-' . $filter_group_filter['slug'];
                             ?>
-
                             <div class="map__filters__filter">
-                                <input type="checkbox" name="<?= $filter_field_name; ?>"
-                                        value="<?= $filter_group_filter['slug']; ?>" id="<?= $filter_field_id; ?>">
+                                <input
+                                    type="checkbox"
+                                    name="<?= esc_attr($filter_field_name); ?>"
+                                    value="<?= esc_attr($filter_group_filter['slug']); ?>"
+                                    id="<?= esc_attr($filter_field_id); ?>">
 
                                 <label
-                                    class="map__filters__filter__label map__filters__filter--<?= $filter_group_filter['slug']; ?>"
-                                    for="<?= $filter_field_id; ?>">
-                                    <?php if ($filter_group['icons']) { ?>
-                                        <span aria-hidden="true"
-                                                class="map__icon map__icon--<?= $filter_group_filter['slug']; ?>">
-                                        </span>
-                                    <?php } ?>
+                                    class="<?= \Granola\Helpers::build_classes([
+                                        'map__filters__filter__label',
+                                        'map__filters__filter--' . $filter_group_filter['slug'],
+                                    ]); ?>"
+                                    for="<?= esc_attr($filter_field_id); ?>">
                                     <span class="map__filters__filter__text">
-                                        <?= $filter_group_filter['label']; ?>
+                                        <?= esc_html($filter_group_filter['label']); ?>
                                     </span>
-
                                 </label>
                             </div>
                         <?php } ?>
@@ -46,6 +49,7 @@
             <?php if (isset($args['buttons']['apply'])) {?>
                 <?= Granola\Component::get('button', $args['buttons']['apply']); ?>
             <?php } ?>
+
             <?= Granola\Component::get('button', $args['buttons']['clear']); ?>
         </div>
     </form>
