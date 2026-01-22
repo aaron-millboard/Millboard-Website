38de23184ae68366f195dc2f09319a080abcb0f5
Tommy Ferry <tommy@wholegraindigital.com>
2026-01-22 12:06:45 +0000

    add Advice Centre post type

---
+++ Theme/PostTypes/AdviceCentre.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * Registers 'Advice Centre' CPT & handles related functionality.
+ */
+
+namespace Theme\PostTypes;
+
+class AdviceCentre
+{
+    protected const SLUG = 'advice-centre';
+
+    public static function init(): void
+    {
+        \add_action('init', [__CLASS__, 'register_post_type']);
+        // \add_action('acf/init', [__CLASS__, 'add_settings_page']);
+        \add_filter('granola/templates/post-types', [__CLASS__, 'filter_granola_templates_post_types']);
+    }
+
+    /**
+     * Register CPT.
+     *
+     * @link https://github.com/johnbillion/extended-cpts/wiki/Registering-Post-Types
+     */
+    public static function register_post_type(): void
+    {
+        if (!function_exists('register_extended_post_type')) {
+            return;
+        }
+
+        \register_extended_post_type(self::SLUG, [
+            // Core post type configuration.
+            'public' => true,
+            'has_archive' => false,
+            'hierarchical' => true,
+            'show_in_rest' => true,
+            'menu_position' => 25, // Below comments.
+            'menu_icon' => 'dashicons-format-status',
+            'supports' => [
+                'title',
+                'editor',
+                'excerpt',
+                'revisions',
+                'thumbnail',
+                'author',
+                'custom-fields',
+            ],
+            'taxonomies' => [],
+            'template' => [
+                [
+                    'core/paragraph',
+                    [
+                        'placeholder' => 'Add content...',
+                    ]
+                ],
+            ],
+
+            // Extended post type configuration.
+            'admin_filters' => [],
+            'admin_cols' => [
+                'thumbnail' => [
+                    'title'          => 'Thumbnail',
+                    'featured_image' => 'thumbnail',
+                    'width'          => 80,
+                    'height'         => 80,
+                ],
+                'title' => [
+                    'title' => 'Title',
+                ],
+                'author' => [
+                    'title' => 'Author',
+                ],
+                'updated' => [
+                    'title'      => 'Updated',
+                    'post_field' => 'post_modified',
+                    'date_format' => 'Y/m/d \a\t H:i a',
+                ],
+            ],
+        ], [
+            // Override the base names used for labels (optional).
+            'singular' => \__('Advice Article', 'granola'),
+            'plural'   => \__('Advice Articles', 'granola'),
+            'slug'     => self::SLUG,
+        ]);
+    }
+
+    /**
+     * Adds an ACF settings page for this post type.
+     */
+    public static function add_settings_page(): void
+    {
+        if (!function_exists('acf_add_options_sub_page')) {
+            return;
+        }
+
+        \acf_add_options_sub_page([
+            'page_title'  => \__('Advice Articles Settings', 'granola'),
+            'menu_title'  => \__('Advice Articles Settings', 'granola'),
+            'menu_slug'   => 'acf-options-advice-articles-settings',
+            'parent_slug' => 'edit.php?post_type=' . self::SLUG,
+        ]);
+    }
+
+    /**
+     * Filter the Granola Templates Post Types array to enable Template Pages for this post type.
+     *
+     * @see /Granola/WordPress/TemplatePage.php
+     *
+     * @return array The filtered post type array.
+     */
+    public static function filter_granola_templates_post_types($post_types)
+    {
+        $post_types[] = self::SLUG;
+        return $post_types;
+    }
+}
