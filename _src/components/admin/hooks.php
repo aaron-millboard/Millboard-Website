64061d38d88bc9c8c70e61e438db745ee9e4c22e
Tommy Ferry <tommy@wholegraindigital.com>
2026-01-22 15:51:19 +0000

    upate admin component styles

--- _src/components/admin/hooks.php
+++ _src/components/admin/hooks.php
@@ -4,5 +4,3 @@ namespace Granola\Components\Admin;
 
 \add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_admin_bar_styles');
 \add_action('admin_enqueue_scripts', __NAMESPACE__ . '\\enqueue_admin_bar_styles');
-
-\add_action('admin_enqueue_scripts', __NAMESPACE__ . '\\enqueue_admin_styles');
