67bbd50dedfd6a57507d420b96ccde066b8c2ca3
Tommy Ferry <tommy@wholegraindigital.com>
2026-01-22 17:11:50 +0000

    add product block to products by default

--- Theme/PostTypes/Product.php
+++ Theme/PostTypes/Product.php
@@ -13,6 +13,20 @@ class Product
     public static function init(): void
     {
         \add_filter('use_block_editor_for_post_type', [__CLASS__, 'activate_gutenberg_block_editor'], 10, 2);
+        \add_filter('register_post_type_args', [__CLASS__, 'filter_register_post_type_args'], 10, 2);
+    }
+
+    public static function filter_register_post_type_args($args, $post_type)
+    {
+        if ($post_type !== self::SLUG) {
+            return $args;
+        }
+
+        $args['template'] = [
+            ['acf/wc-single-product'],
+        ];
+
+        return $args;
     }
 
     public static function activate_gutenberg_block_editor($can_edit, $post_type)
