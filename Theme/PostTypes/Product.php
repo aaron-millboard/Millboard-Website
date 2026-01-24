d106cdf5bc2d31a6f245eadc8f8a0afded1ab737
Tommy Ferry <tommy@wholegraindigital.com>
2026-01-24 19:34:29 +0000

    add a product variation finding function

--- Theme/PostTypes/Product.php
+++ Theme/PostTypes/Product.php
@@ -69,4 +69,60 @@ class Product
             \wc_get_product_id_by_sku($sku)
         );
     }
+
+    /**
+     * Retrieve a product variation that matches a specific set of attribute values.
+     *
+     * Returns the first match if multiple products would match.
+     *
+     * @param integer|\WC_Product $product
+     * @param array $attributes
+     * @return array|null
+     */
+    public static function get_product_variation_by_attributes(int|\WC_Product $product, array $attributes = []): ?array
+    {
+        // Retrieve full Product object.
+        if (\is_int($product)) {
+            /** @var \WC_Product $product */
+            $product = \wc_get_product($product);
+        }
+
+        // Bail early - only relevant for variable products.
+        if (!$product->is_type('variable')) {
+            return $product->get_data();
+        }
+
+        // Filter out invalid attributes.
+        $valid_attributes = array_keys($product->get_attributes());
+        $attributes = array_filter($attributes, function ($attribute_value, $attribute_key) use ($valid_attributes) {
+            return in_array($attribute_key, $valid_attributes, true);
+        }, \ARRAY_FILTER_USE_BOTH);
+
+        // Bail early - no valid attributes to check.
+        if (empty($attributes)) {
+            return null;
+        }
+
+        $variations = $product->get_available_variations();
+
+        // Search for matching variation, returning the first found.
+        $found_product = \array_find($variations, function ($variation) use ($attributes) {
+            $matches = true;
+
+            foreach ($attributes as $attribute_key => $attribute_value) {
+                if ($variation['attributes']['attribute_' . $attribute_key] !== $attribute_value) {
+                    $matches = false;
+                    break;
+                }
+            }
+
+            return $matches;
+        });
+
+        if (is_array($found_product)) {
+            return $found_product;
+        }
+
+        return null;
+    }
 }
