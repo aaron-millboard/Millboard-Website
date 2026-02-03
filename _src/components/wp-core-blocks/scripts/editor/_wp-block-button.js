6d7e5b2380e24296ab3a650b30fb7fdc3d3f7092
Yehor Cherepin <yehorcherepin@macbookpro.lan>
2026-02-03 11:02:05 +0000

    Buttons hover state + secondary style

--- _src/components/wp-core-blocks/scripts/editor/_wp-block-button.js
+++ _src/components/wp-core-blocks/scripts/editor/_wp-block-button.js
@@ -2,8 +2,8 @@
  * Unregister default styles for core/button block.
  */
 wp.domReady(() => {
-    wp.blocks.unregisterBlockStyle('core/button', 'default');
+    // wp.blocks.unregisterBlockStyle('core/button', 'default'); // Keep default style
     wp.blocks.unregisterBlockStyle('core/button', 'fill');
-    wp.blocks.unregisterBlockStyle('core/button', 'outline');
+    // wp.blocks.unregisterBlockStyle('core/button', 'outline'); // Keep outline style
     wp.blocks.unregisterBlockStyle('core/button', 'squared');
 });
