aec9370d7e84c232f6747a224044aacd8ee4f9e0
Tommy Ferry <tommy@wholegraindigital.com>
2026-02-04 12:32:01 +0000

    fix issue with missing ID in woocommerce payment template

--- woocommerce/checkout/payment.php
+++ woocommerce/checkout/payment.php
@@ -22,20 +22,23 @@ if (! wp_doing_ajax()) {
     do_action('woocommerce_review_order_before_payment');
 }
 ?>
-
-<?php if (WC()->cart && WC()->cart->needs_payment()) : ?>
-    <?php
-    if (! empty($available_gateways)) {
-        foreach ($available_gateways as $gateway) {
-            wc_get_template('checkout/payment-method.php', array( 'gateway' => $gateway ));
-        }
-    } else {
-        echo '<div>';
-        wc_print_notice(apply_filters('woocommerce_no_available_payment_methods_message', WC()->customer->get_billing_country() ? esc_html__('Sorry, it seems that there are no available payment methods. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce') : esc_html__('Please fill in your details above to see available payment methods.', 'woocommerce')), 'notice'); // phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
-        echo '</div>';
-    }
-    ?>
-<?php endif; ?>
+<div id="payment" class="woocommerce-checkout-payment">
+    <?php if (WC()->cart && WC()->cart->needs_payment()) : ?>
+        <div class="wc_payment_methods payment_methods methods">
+            <?php
+            if (! empty($available_gateways)) {
+                foreach ($available_gateways as $gateway) {
+                    wc_get_template('checkout/payment-method.php', array( 'gateway' => $gateway ));
+                }
+            } else {
+                echo '<div>';
+                wc_print_notice(apply_filters('woocommerce_no_available_payment_methods_message', WC()->customer->get_billing_country() ? esc_html__('Sorry, it seems that there are no available payment methods. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce') : esc_html__('Please fill in your details above to see available payment methods.', 'woocommerce')), 'notice'); // phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
+                echo '</div>';
+            }
+            ?>
+        </ul>
+    <?php endif; ?>
+</div>
 
 <noscript>
     <?php
