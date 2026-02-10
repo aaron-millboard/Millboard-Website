7dbee01a77194128624a9b2ae94d42f4341783ba
Tommy Ferry <tommy@wholegraindigital.com>
2026-02-10 16:42:27 +0000

    tidy up MAp.js file

--- _src/components/map/scripts/Map.js
+++ _src/components/map/scripts/Map.js
@@ -38,6 +38,7 @@ class Map {
         this.LMAP_MIN_ZOOM = 2.4;
         this.LMAP_MAX_ZOOM = 15; // Was 7.2
         this.LMAP_INITIAL_CENTER = [55, -5]; // Move center to UK.
+        this.LMAP_DISTANCE_CENTER = L.latLng(52.3, -1.4);
 
         this.LMAP_MARKER_WIDTH = 31;
         this.LMAP_MARKER_HEIGHT = 40;
@@ -53,303 +54,9 @@ class Map {
     }
 
     init() {
-        this.LMAP_DISTANCE_CENTER = L.latLng(52.3, -1.4);
-
         this.initLeafletMap();
         this.initLeafletMarkers();
         this.initSearch();
-
-        // // Init UI interactivity.
-        // this.initFilterClearApplyButtons();
-
-        // // Do a filter/search.
-        // this.triggerFormFilter(false);
-    }
-
-    /**
-     * Handles filtering the data.
-     */
-    triggerFormFilter(updateBounds = true) {
-        // Get form data.
-        const formData = new FormData(this.filterFormEl);
-        const selectedFilterSlugsByFiltergroup = {};
-        // let totalFiltersApplied = 0; // Increments with each filter added.
-        let totalFilterGroupsApplied = 0;
-
-        // Handle search.
-        if (formData.has('map--map--search')) {
-            const searchTerm = formData.get('map--map--search');
-            if (searchTerm) {
-                this.handleSearch(searchTerm.toLowerCase());
-
-                return;
-            }
-        }
-
-        // Find the value that has been filtered by.
-        // Loop over the filter slugs by filter groups.
-        // Check the form data submited contains one of our filter groups.
-        [...Object.keys(this.appliedFilterSlugsByFiltergroup)].forEach((filtergroupIDPart) => {
-            const filtergroupID = `map-filter-${filtergroupIDPart}`;
-
-            if (formData.has(filtergroupID)) {
-                selectedFilterSlugsByFiltergroup[filtergroupID] = formData.getAll(filtergroupID);
-
-                // If the formData value is not empty, incremenet our groups count by 1.
-                if (formData.get(filtergroupID).length > 0) {
-                    totalFilterGroupsApplied += 1;
-                }
-            } else {
-                selectedFilterSlugsByFiltergroup[filtergroupID] = [];
-            }
-
-            // An incrementor for each filter added.
-            // totalFiltersApplied += selectedFilterSlugsByFiltergroup[filtergroupID].length;
-        });
-
-        // We now have a bunch of things we want to filter by.
-        // Clear existing layers.
-        // this.filteredMarkersSubGroup.clearLayers();
-        this.filteredMarkersFlatGroup.clearLayers();
-        const tableResults = [];
-
-        // Loop over all our markers.
-        // this.allMarkersSubGroup.getLayers().forEach((markerLayer) => {
-        this.allMarkersFlatGroup.getLayers().forEach((markerLayer) => {
-            // console.log('markerLayer options', markerLayer.options);
-
-            let countOfshowForThisFilterNameForAllFilters = 0;
-
-            // For marker X
-            // Check each of the selected filters.
-            // Loop over the filters selected by the user.
-            // Gather data on what has been added.
-            Object.keys(selectedFilterSlugsByFiltergroup).forEach((filterName) => {
-                // Loop over one filter name.
-                // This is an OR relationship - we must find one match.
-                let showForThisFilterName = false;
-                const chosenFilterValues = selectedFilterSlugsByFiltergroup[filterName];
-                const filterNameShort = filterName.replace('map-filter-', '');
-                const markerMetaData = markerLayer.options.options;
-
-                // Loop over the array of chosen values for this filter - e.g. "country".
-                if (chosenFilterValues.length > 0) {
-                    chosenFilterValues.forEach((filterValue) => {
-                        if (markerMetaData[filterNameShort] === filterValue) {
-                            showForThisFilterName = true;
-                        } else if (filterNameShort === 'energytype') {
-                            // Handle energy type - now an array of energy types (wind, solar).
-                            const jsonString = JSON.parse(markerMetaData[filterNameShort]);
-
-                            // Handle differences between slug and value.
-                            let filterValueToCheck = filterValue;
-                            if (filterValue === 'waste-to-energy') {
-                                filterValueToCheck = 'waste_to_energy';
-                            }
-                            if (jsonString.includes(filterValueToCheck)) {
-                                showForThisFilterName = true;
-                            }
-                        }
-                    });
-                }
-
-                if (showForThisFilterName) {
-                    countOfshowForThisFilterNameForAllFilters += 1;
-                }
-            });
-
-            if (countOfshowForThisFilterNameForAllFilters === totalFilterGroupsApplied) {
-                // this.filteredMarkersSubGroup.addLayer(markerLayer);
-                this.filteredMarkersFlatGroup.addLayer(markerLayer);
-                tableResults.push(markerLayer.options.themeData.tableDataRowElement);
-            }
-        });
-
-        // this.lmap.addLayer(this.filteredMarkersSubGroup);
-        this.lmap.addLayer(this.filteredMarkersFlatGroup);
-
-        // if (this.filteredMarkersSubGroup.getLayers().length > 0) {
-        if (this.filteredMarkersFlatGroup.getLayers().length > 0 && updateBounds) {
-            const filteredMarkersBounds = this.filteredMarkersFlatGroup.getBounds();
-            this.lmap.setMaxZoom(6.5); // Limit inital zoom
-            this.lmap.fitBounds(filteredMarkersBounds);
-
-            // Return max zoom to its initial value.
-            setTimeout(() => {
-                this.lmap.setMaxZoom(this.LMAP_MAX_ZOOM);
-            }, 100);
-        } else if (!updateBounds) {
-            // Force initial map position.
-            setTimeout(() => {
-                // this.lmap.setZoom(4.199999999999999);
-                this.lmap.setZoom(4.55);
-            }, 100);
-        }
-    }
-
-    /**
-     * Handles any searches
-     *
-     * @param searchTerm - The searched for term from the search input.value.
-     */
-    handleSearch(searchTerm) {
-        // Clear existing layers.
-        // this.filteredMarkersSubGroup.clearLayers();
-        this.filteredMarkersFlatGroup.clearLayers();
-        const tableResults = [];
-
-        // Loop over markers and check title for name.
-        this.allMarkersFlatGroup.getLayers().forEach((markerLayer) => {
-            // this.allMarkersSubGroup.getLayers().forEach((markerLayer) => {
-            // const titleOfPlant = markerLayer.options.title.trim().toLowerCase();
-            const textContentOfTableRow = markerLayer.options.themeData.tableDataRowElement.textContent
-                .trim()
-                .toLowerCase();
-
-            if (textContentOfTableRow.indexOf(searchTerm) !== -1) {
-                // this.filteredMarkersSubGroup.addLayer(markerLayer);
-                this.filteredMarkersFlatGroup.addLayer(markerLayer);
-
-                tableResults.push(markerLayer.options.themeData.tableDataRowElement);
-            }
-        });
-
-        // Update map.
-        this.lmap.addLayer(this.filteredMarkersFlatGroup);
-
-        if (this.filteredMarkersFlatGroup.getLayers().length > 0) {
-            const filteredMarkersBounds = this.filteredMarkersFlatGroup.getBounds();
-            this.lmap.fitBounds(filteredMarkersBounds);
-        }
-    }
-
-    static getDataFromRowElement(element) {
-        return {
-            lat: element.dataset.mapItemLat,
-            lng: element.dataset.mapItemLng,
-            name: element.querySelector('.map__listing__title').textContent,
-        };
-    }
-
-    isMobileViewport() {
-        return isElementVisible(this.mobileMediaQueryRefEl);
-    }
-
-    /**
-     * Leaflet Markers setup.
-     */
-    initLeafletMarkers() {
-        const markerHoizontalMiddle = this.LMAP_MARKER_WIDTH / 2; // Icon width / 2 to center it.
-
-        this.listingEls.forEach((el) => {
-            // Get data.
-            const rowData = Map.getDataFromRowElement(el);
-
-            // Get data values.
-            const rowLatLng = L.latLng(rowData.lat, rowData.lng);
-            const rowLat = parseFloat(rowData.lat);
-            const rowLng = parseFloat(rowData.lng);
-            const rowTitle = rowData.name;
-
-            let markerHtml = `<span class="leaflet-marker-icon__icon-container" aria-hidden="true">`;
-            markerHtml += `<span class="leaflet-marker-icon__icon"></span>`;
-            markerHtml += `<span class="screen-reader-text">${rowTitle}</span>`;
-            markerHtml += ' </span>';
-
-            // https://leafletjs.com/reference.html#marker
-            const marker = L.marker([rowLat, rowLng], {
-                autoPanOnFocus: true,
-                icon: L.divIcon({
-                    html: markerHtml,
-                    iconSize: [this.LMAP_MARKER_WIDTH, this.LMAP_MARKER_HEIGHT],
-                    iconAnchor: [markerHoizontalMiddle, this.LMAP_MARKER_HEIGHT], // Icon radius / 2 to center it.
-                }),
-
-                // Custom object data.
-                themeData: {
-                    tableDataRowElement: el,
-                },
-            });
-
-            const METERS_TO_MILES_RATIO = 0.000621371;
-            const distMeters = rowLatLng.distanceTo(this.LMAP_DISTANCE_CENTER);
-            const distMiles = Math.round(METERS_TO_MILES_RATIO * distMeters * 100) / 100; // 2 decimal points.
-
-            const rowMeta = el.querySelector('.map__listing__meta');
-
-            if (rowMeta) {
-                const distEl = document.createElement('span');
-                distEl.classList.add('map__listing__distance');
-                distEl.textContent = distMiles + ' miles'; // TODO: translate
-                rowMeta.appendChild(distEl);
-            }
-
-            // Add the marker to the leaflet layer.
-            // this.allMarkersSubGroup.addLayer(marker);
-            this.allMarkersFlatGroup.addLayer(marker);
-
-            // /* eslint-disable-next-line no-underscore-dangle -- We need this. */
-            // el.setAttribute('data-map-leaflet-id', this.allMarkersSubGroup.getLayerId(marker));
-            el.setAttribute('data-map-leaflet-id', this.allMarkersFlatGroup.getLayerId(marker));
-        });
-
-        // Add all markers sub groups to map.
-        // this.lmap.addLayer(this.allMarkersSubGroup);
-        this.lmap.addLayer(this.allMarkersFlatGroup);
-    }
-
-    /**
-     * Inits the filter event listeners.
-     */
-    initFilters() {
-        this.filterEls.forEach((el) => {
-            this.markerSubGroupsByFilterableValue[el.value] = new LFeatureGroupSubGroup(this.TEMPmarkersClusterGroup);
-
-            el.addEventListener('change', (event) => {
-                event.preventDefault();
-                this.triggerFormFilter();
-            });
-        });
-    }
-
-    /**
-     * Inits the filter event listeners.
-     */
-    initSearch() {
-        if (!this.searchInput || !this.searchSubmit) {
-            return;
-        }
-
-       this.searchSubmit.addEventListener('click', async (event) => {
-            event.preventDefault();
-            const searchQuery = this.searchInput?.value;
-
-            // Bail early - invalid query: reset view.
-            if (!searchQuery) {
-                this.resetMapView();
-                return;
-            }
-
-            const response = await this.newRequest(searchQuery);
-
-            // Bail early - no valid response found.
-            if (!response || !response.results[0]) {
-                this.resetMapView();
-                console.warn('Invalid response for geocoding query.');
-                return;
-            }
-
-            const {lat, lng} = response.results[0].geometry.location;
-            this.lmap.setView(new L.LatLng(lat, lng), 8);
-        });
-    }
-
-    resetMapView() {
-        this.lmap.setView(new L.LatLng(
-            this.LMAP_INITIAL_CENTER[0],
-            this.LMAP_INITIAL_CENTER[1]),
-            this.LMAP_INITIAL_ZOOM
-        );
     }
 
     /**
@@ -410,16 +117,158 @@ class Map {
         // });
     }
 
-    initFilterClearApplyButtons() {
-        this.filtersClearButton.addEventListener('click', (e) => {
-            e.preventDefault();
+    /**
+     * Leaflet Markers setup.
+     */
+    initLeafletMarkers() {
+        const markerHoizontalMiddle = this.LMAP_MARKER_WIDTH / 2; // Icon width / 2 to center it.
 
-            // Reset.
-            this.filterFormEl.reset();
+        this.listingEls.forEach((el) => {
+            // Get data.
+            const rowData = Map.getDataFromRowElement(el);
 
-            // Do a filter/search.
-            this.triggerFormFilter();
+            // Get data values.
+            const rowLatLng = L.latLng(rowData.lat, rowData.lng);
+            const rowLat = parseFloat(rowData.lat);
+            const rowLng = parseFloat(rowData.lng);
+            const rowTitle = rowData.name;
+
+            let markerHtml = `<span class="leaflet-marker-icon__icon-container" aria-hidden="true">`;
+            markerHtml += `<span class="leaflet-marker-icon__icon"></span>`;
+            markerHtml += `<span class="screen-reader-text">${rowTitle}</span>`;
+            markerHtml += ' </span>';
+
+            // https://leafletjs.com/reference.html#marker
+            const marker = L.marker([rowLat, rowLng], {
+                autoPanOnFocus: true,
+                icon: L.divIcon({
+                    html: markerHtml,
+                    iconSize: [this.LMAP_MARKER_WIDTH, this.LMAP_MARKER_HEIGHT],
+                    iconAnchor: [markerHoizontalMiddle, this.LMAP_MARKER_HEIGHT], // Icon radius / 2 to center it.
+                }),
+
+                // Custom object data.
+                themeData: {
+                    tableDataRowElement: el,
+                },
+            });
+
+            const METERS_TO_MILES_RATIO = 0.000621371;
+            const distMeters = rowLatLng.distanceTo(this.LMAP_DISTANCE_CENTER);
+            const distMiles = Math.round(METERS_TO_MILES_RATIO * distMeters * 100) / 100; // 2 decimal points.
+
+
+
+            const rowMeta = el.querySelector('.map__listing__meta');
+
+            if (rowMeta) {
+                const distEl = document.createElement('span');
+                distEl.classList.add('map__listing__distance');
+                distEl.textContent = distMiles + ' miles'; // TODO: translate
+                rowMeta.appendChild(distEl);
+            }
+
+            // Add the marker to the leaflet layer.
+            // this.allMarkersSubGroup.addLayer(marker);
+            this.allMarkersFlatGroup.addLayer(marker);
+
+            // /* eslint-disable-next-line no-underscore-dangle -- We need this. */
+            // el.setAttribute('data-map-leaflet-id', this.allMarkersSubGroup.getLayerId(marker));
+            el.setAttribute('data-map-leaflet-id', this.allMarkersFlatGroup.getLayerId(marker));
         });
+
+        // Add all markers sub groups to map.
+        // this.lmap.addLayer(this.allMarkersSubGroup);
+        this.lmap.addLayer(this.allMarkersFlatGroup);
+    }
+
+    /**
+     * Inits the filter event listeners.
+     */
+    initSearch() {
+        if (!this.searchInput || !this.searchSubmit) {
+            return;
+        }
+
+       this.searchSubmit.addEventListener('click', async (event) => {
+            event.preventDefault();
+            const searchQuery = this.searchInput?.value;
+
+            // Bail early - invalid query: reset view.
+            if (!searchQuery) {
+                this.resetMapView();
+                return;
+            }
+
+            const response = await this.newRequest(searchQuery);
+
+            // Bail early - no valid response found.
+            if (!response || !response.results[0]) {
+                this.resetMapView();
+                console.warn('Invalid response for geocoding query.');
+                return;
+            }
+
+            const {lat, lng} = response.results[0].geometry.location;
+            this.lmap.setView(new L.LatLng(lat, lng), 8);
+        });
+    }
+
+    /**
+     * Handles any searches
+     *
+     * @param searchTerm - The searched for term from the search input.value.
+     */
+    handleSearch(searchTerm) {
+        // Clear existing layers.
+        // this.filteredMarkersSubGroup.clearLayers();
+        this.filteredMarkersFlatGroup.clearLayers();
+        const tableResults = [];
+
+        // Loop over markers and check title for name.
+        this.allMarkersFlatGroup.getLayers().forEach((markerLayer) => {
+            // this.allMarkersSubGroup.getLayers().forEach((markerLayer) => {
+            // const titleOfPlant = markerLayer.options.title.trim().toLowerCase();
+            const textContentOfTableRow = markerLayer.options.themeData.tableDataRowElement.textContent
+                .trim()
+                .toLowerCase();
+
+            if (textContentOfTableRow.indexOf(searchTerm) !== -1) {
+                // this.filteredMarkersSubGroup.addLayer(markerLayer);
+                this.filteredMarkersFlatGroup.addLayer(markerLayer);
+
+                tableResults.push(markerLayer.options.themeData.tableDataRowElement);
+            }
+        });
+
+        // Update map.
+        this.lmap.addLayer(this.filteredMarkersFlatGroup);
+
+        if (this.filteredMarkersFlatGroup.getLayers().length > 0) {
+            const filteredMarkersBounds = this.filteredMarkersFlatGroup.getBounds();
+            this.lmap.fitBounds(filteredMarkersBounds);
+        }
+    }
+
+    // Helpers
+    static getDataFromRowElement(element) {
+        return {
+            lat: element.dataset.mapItemLat,
+            lng: element.dataset.mapItemLng,
+            name: element.querySelector('.map__listing__title').textContent,
+        };
+    }
+
+    isMobileViewport() {
+        return isElementVisible(this.mobileMediaQueryRefEl);
+    }
+
+    resetMapView() {
+        this.lmap.setView(new L.LatLng(
+            this.LMAP_INITIAL_CENTER[0],
+            this.LMAP_INITIAL_CENTER[1]),
+            this.LMAP_INITIAL_ZOOM
+        );
     }
 
     async newRequest(data) {
