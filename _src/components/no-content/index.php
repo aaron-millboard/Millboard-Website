28136e46179fef1fe4d131429ee8defbc78bd515
Tommy Ferry <tommy@wholegraindigital.com>
2026-02-11 15:49:24 +0000

    fix no-content alignment

--- _src/components/no-content/index.php
+++ _src/components/no-content/index.php
@@ -1,4 +1,4 @@
-<section class="no-content">
+<section class=" alignwide">
     <p class="no-content__message is-style-typestyle-h4">
         <?= wp_kses_post($args['content']['message']); ?>
     </p>
