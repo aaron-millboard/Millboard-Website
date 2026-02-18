7e65d125ba7da5e4e983b0498cfd2c5b2b779fdb
Tommy Ferry <tommy@wholegraindigital.com>
2026-02-18 14:18:52 +0000

    add simple product price to template

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -47,6 +47,16 @@ if ($product->is_in_stock()) : ?>
         ?>
 
         <div class="product__add-to-cart-wrapper">
+            <div class="woocommerce-simple">
+                <div class="woocommerce-simple-price">
+                    <?= $product->get_price_html(); ?>
+                </div>
+
+                <div class="woocommerce-simple-tax">
+                    <?= esc_html__('Excl VAT', 'granola'); ?>
+                </div>
+            </div>
+
             <button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="single_add_to_cart_button button alt<?php echo esc_attr(wc_wp_theme_get_element_class_name('button') ? ' ' . wc_wp_theme_get_element_class_name('button') : ''); ?>"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
         </div>
 
