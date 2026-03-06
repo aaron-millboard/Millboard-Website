01b2a8e2d7f8e3927e3091a2d4b6c0d28230f5f3
Tommy Ferry <tommy@wholegraindigital.com>
2026-03-06 15:50:38 +0000

    improve website selector image sizing

--- _src/components/website-selector/template/functions.php
+++ _src/components/website-selector/template/functions.php
@@ -33,7 +33,7 @@ function filter_args(array $args): ?array
             if (!empty($column['image'])) {
                 $args['columns'][$index]['image_data'] = [
                     'attachment_id' => $column['image'],
-                    'size' => 'medium',
+                    'size' => 'large',
                     'fetchpriority' => 'high',
                     'data-spai-eager' => true,
                     'loading' => 'eager',
