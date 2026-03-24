076b11ce852f98ba6f19077596610b937f835be6
Tommy Ferry <tommy@wholegraindigital.com>
2026-03-24 11:54:16 +0000

    add better sample checks to cart and checkout

--- _src/components/wc-cart/index.php
+++ _src/components/wc-cart/index.php
@@ -70,13 +70,13 @@ do_action('woocommerce_before_cart');?>
 
                 if ($sample_size_attribute || $board_width_attribute || $calculator_enabled) {
                     // If board, we check by 3 signs: board width attribute, sample size attribute set to full or calculator enabled
-                    if ($board_width_attribute || $sample_size_attribute === 'Full' || $calculator_enabled) {
+                    if ($board_width_attribute || $calculator_enabled || !\Theme\WooCommerce\Utils::is_sample($_product)) {
                         $unit_name_singular = __('board', 'granola');
                         $unit_name_plural = __('boards', 'granola');
                     }
 
                     // override if we have any sign of sample size
-                    if ($sample_size_attribute === 'Small' || $sample_size_attribute === 'Large') {
+                    if (\Theme\WooCommerce\Utils::is_sample($_product)) {
                         $unit_name_singular = __('sample', 'granola');
                         $unit_name_plural = __('samples', 'granola');
                     }
