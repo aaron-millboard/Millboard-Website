efbdbfe9f71fefaa6e593d8d4fe412342aa2f8fd
George Lawrence <george@wholegraindigital.com>
2026-04-01 14:38:53 +0100

    add taxonomy filters

--- _src/components/template-loop/index.php
+++ _src/components/template-loop/index.php
@@ -1,8 +1,6 @@
-<?php if (!empty($args['items_component_args']['items'])) { ?>
-    <?php if (!empty($args['taxonomy_filters'])) { ?>
-        <?= \Granola\Component::get('taxonomy-filters', $args['taxonomy_filters']); ?>
-    <?php } ?>
+<?= \Granola\Component::get('taxonomy-filters', $args['taxonomy_filters_args']); ?>
 
+<?php if (!empty($args['items_component_args']['items'])) { ?>
     <?= \Granola\Component::get($args['items_component'], $args['items_component_args']); ?>
     <?= \Granola\Component::get('pagination'); ?>
 <?php } else { ?>
