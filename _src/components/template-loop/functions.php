38adfec9538182ead08766762e343faf377a3ee4
George Lawrence <george@wholegraindigital.com>
2026-04-01 13:50:19 +0100

    taxonomy filters

--- _src/components/template-loop/functions.php
+++ _src/components/template-loop/functions.php
@@ -41,9 +41,62 @@ function filter_args(array $args): ?array
     } else {
         $post_type = \get_post_type();
     }
-    $args['items_component_args']['post_type'] = $post_type;
 
-    // Set limit to 12
+    // Handle URL-based filtering
+    $paged = (\get_query_var('paged')) ? \get_query_var('paged') : 1;
+    $taxonomy = $args['taxonomy'];
+    
+    $query_args = [
+        'post_type' => $post_type,
+        'posts_per_page' => 12,
+        'post_status' => 'publish',
+        'paged' => $paged,
+    ];
+
+    // Filter by taxonomy if provided in URL
+    if (isset($_GET[$taxonomy]) && !empty($_GET[$taxonomy])) {
+        $terms = explode(' ', \sanitize_text_field($_GET[$taxonomy]));
+
+        foreach ($terms as $term) {
+            $query_args['tax_query'][] = [
+                [
+                    'taxonomy' => $taxonomy,
+                    'field' => 'slug',
+                    'terms' => $term,
+                ],
+            ];
+        }
+
+        if (count($terms) > 1) {
+            $query_args['tax_query'][]['relation'] = 'AND';
+        }
+    }
+
+    // Run our query only if not already populated
+    if (empty($args['items'])) {
+        $query = new \WP_Query($query_args);
+        $args['items'] = [];
+        foreach ($query->posts as $post) {
+            $args['items'][]['object'] = $post;
+        }
+        $max_num_pages = $query->max_num_pages;
+    } else {
+        $max_num_pages = 1;
+    }
+
+    // Set default taxonomy filter arguments.
+    $args['taxonomy_filters_args'] = [
+        'label' => $args['filter_label'],
+        'taxonomy' => $args['taxonomy'],
+        'object' => $args['object'],
+        'show_images' => false,
+        'preserve_url' => true,
+    ];
+
+    // Fill items into items component args.
+    $args['items_component_args']['items'] = $args['items'];
+    $args['items_component_args']['wp_query'] = false;
+    $args['items_component_args']['post_type'] = $post_type;
     $args['items_component_args']['limit'] = 12;
 
     // Set columns to 3 if not already set
@@ -59,6 +112,12 @@ function filter_args(array $args): ?array
         $args['items_component_args']['columns'] = 4;
         unset($arg['taxonomy_filters']);
     }
+
+    // Set up pagination args.
+    $args['pagination_args'] = [
+        'paged' => $paged,
+        'max_num_pages' => $max_num_pages,
+    ];
 
     // Filterable items output component.
     $args['items_component'] = \apply_filters('granola/components/template-loop/items-component', 'cards-automatic');
--- _src/components/template-loop/index.php
+++ _src/components/template-loop/index.php
@@ -2,7 +2,7 @@
 
 <?php if (!empty($args['items_component_args']['items'])) { ?>
     <?= \Granola\Component::get($args['items_component'], $args['items_component_args']); ?>
-    <?= \Granola\Component::get('pagination'); ?>
+    <?= \Granola\Component::get('pagination', $args['pagination_args']); ?>
 <?php } else { ?>
     <?= \Granola\Component::get('no-content', [
         'object' => $args['object'],
