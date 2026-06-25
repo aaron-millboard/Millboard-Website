dac9c90451ba8cc0a9e9f70ae25fbb53cd61ba12
George Lawrence <george@wholegraindigital.com>
2026-06-25 07:28:38 +0000

    fix email headers
    (cherry picked from commit 7067d793d8bee329fd65ca6e21776c19582269a7)
    
    Co-authored-by: George Lawrence <george@wholegraindigital.com>

--- Theme/WooCommerce/QuoteShare.php
+++ Theme/WooCommerce/QuoteShare.php
@@ -584,7 +584,7 @@ class QuoteShare
             'phone_number' => isset($_POST['phone_number']) ? \sanitize_text_field(\wp_unslash($_POST['phone_number'])) : '',
             'customer_reference_number' => isset($_POST['customer_reference_number']) ? \sanitize_text_field(\wp_unslash($_POST['customer_reference_number'])) : '',
             'sales_notes' => isset($_POST['sales_notes']) ? \sanitize_textarea_field(\wp_unslash($_POST['sales_notes'])) : '',
-            'rep_email_address' => isset($_POST['rep_email_address']) ? \sanitize_email(\wp_unslash($_POST['rep_email_address'])) : ''
+            'rep_email_address' => isset($_POST['rep_email_address']) ? \sanitize_email(\wp_unslash($_POST['rep_email_address'])) : '',
         ];
     }
 
@@ -1280,9 +1280,11 @@ class QuoteShare
         $bcc = $form_data['rep_email_address'] ?? '';
 
         $html_message = self::build_quote_email_html($form_data, $cart_data, $restore_url);
+        $text_message = self::build_quote_email_text($form_data, $cart_data, $restore_url);
 
         $headers = [
             'Content-Type: text/html; charset=UTF-8',
+            'X-Alt-Body: ' . $text_message,
         ];
 
         if (!empty($bcc) && is_email($bcc)) {
