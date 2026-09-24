1e18d449ff789e7c1df060d4a6ff60af265f62c0
Aaron Davis <aaron.davis@millboard.com>
2026-09-24 11:54:11 +0100

    Checkout: send Irish addresses to the office about supply
    There is no online delivery to Ireland, so for a Republic of Ireland
    address or a Northern Ireland (BT) postcode the shipping method section
    now asks the customer to contact the office about supply to Ireland,
    instead of WooCommerce's default telling them to check an address that
    is already correct.

--- _src/components/wc-checkout/hooks.php
+++ _src/components/wc-checkout/hooks.php
@@ -6,3 +6,6 @@ namespace Granola\Components\WC_Checkout;
 
 // Allow shipping section to be updated as part of WC's 'update_order_review' ajax call.
 \add_filter('woocommerce_update_order_review_fragments', __NAMESPACE__ . '\\update_shipping_methods_fragment');
+
+// Point Irish addresses at the office, as there is no online delivery to Ireland.
+\add_filter('woocommerce_no_shipping_available_html', __NAMESPACE__ . '\\ireland_no_shipping_message');
